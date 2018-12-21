8dcd2d5f47901022b606f43c2cab4b3cd38739f8
Tomas Tunys <tunystom@gmail.com>
2018-12-21 15:57:25 +0100

    Bumping version to 1.2.0

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ else:
     poppler_cairo_config = pkgconfig('poppler', 'poppler-glib', 'pycairo', 'cairo')
 
 setup(name='pdfparser-rossum',
-      version='1.1.0',
+      version='1.2.0',
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Intended Audience :: Developers',
                    'Topic :: Text Processing',
