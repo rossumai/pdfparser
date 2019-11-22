89f80f3a3ed4f7e75a3afcb59606ec2509b5cfc9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2019-11-22 13:14:31 +0100

    Update the font installation.
    There was a problem that the PowerPointViewer.exe
    was not available anymore from MS download site so
    we provided a TGZ with the extracted fonts. Now it
    available again via SourceForge.
    
    - do not use the prepared TGZ with fonts
    - update the installation script with new URL for
      PowerPointViewer.exe which is working
    - source of updated vistafonts-installer:
      http://plasmasturm.org/code/vistafonts-installer/vistafonts-installer

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,18 +1,65 @@
 #!/bin/bash
-# Installs pre-packaged msttcorefonts + vistafonts, because originals
-# are not available anymore (see install_fonts_orig.sh).
+#
+# This file is part of pdfparser.
+#
+# pdfparse is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pdfparser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pdfparser.  If not, see <http://www.gnu.org/licenses/>.
+#
+# A part of this code was inspired by vistafonts-installer
+# (http://plasmasturm.org/code/vistafonts-installer/). vistafonts-installer
+# is a free software distributed under the terms of MIT License.
+# For more information about the license see the attached original
+# vistafonts-installer script.
 
 set -e
 
-apt-get install -y fontconfig wget
+apt-get install -y cabextract coreutils debconf fontconfig wget
+
+MS_FONTS_ARCHIVE=IELPKTH.CAB
+MS_FONTS_DIR=/usr/share/fonts/truetype/msttcorefonts/
+
+VISTA_FONTS_ARCHIVE=PowerPointViewer.exe
+VISTA_FONTS_DIR=/usr/share/fonts/truetype/vistafonts/
 
 TMPDIR=`mktemp -d`
 trap 'rm -rf $TMPDIR' EXIT INT QUIT TERM
 cd "$TMPDIR"
 
-wget http://rossum.install.s3-website-eu-west-1.amazonaws.com/msfonts.tar.gz
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
+apt-get install -y ttf-mscorefonts-installer
 
-FONTS_DIR=/usr/share/fonts/truetype
-mkdir -p ${FONTS_DIR}
-tar -xzf msfonts.tar.gz -C ${FONTS_DIR}
-fc-cache -fv ${FONTS_DIR}/{msttcore,vista}fonts/
+wget https://sourceforge.net/projects/corefonts/files/OldFiles/$MS_FONTS_ARCHIVE
+
+if cabextract -L -F 'tahoma*ttf' -d $MS_FONTS_DIR  $MS_FONTS_ARCHIVE
+then
+    chmod 644 $MS_FONTS_DIR/tahoma*
+    fc-cache -fv $MS_FONTS_DIR
+else
+    echo "ERROR: Failed to install Tahoma font!"
+    exit 1
+fi
+
+wget https://sourceforge.net/projects/mscorefonts2/files/cabs/$VISTA_FONTS_ARCHIVE
+
+if cabextract -L -F ppviewer.cab $VISTA_FONTS_ARCHIVE
+then
+    cabextract -L -F '*.TT[FC]' -d $VISTA_FONTS_DIR ppviewer.cab
+
+    ( cd $VISTA_FONTS_DIR && mv cambria.ttc cambria.ttf && chmod 644 \
+            calibri{,b,i,z}.ttf cambria{,b,i,z}.ttf candara{,b,i,z}.ttf \
+            consola{,b,i,z}.ttf constan{,b,i,z}.ttf corbel{,b,i,z}.ttf )
+    fc-cache -fv $VISTA_FONTS_DIR
+else
+    echo "ERROR: Failed to install Vista fonts!"
+    exit 1
+fi
